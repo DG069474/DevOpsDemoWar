206a36c89e74b2aa5ae86801ab6c4e0d8b2ffc01
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-06-25 11:01:24 -0400

    Update TestPalindrome.java

--- src/main/java/TestPalindrome.java
+++ src/main/java/TestPalindrome.java
@@ -1,7 +1,15 @@
 
 public class TestPalindrome {
 	public boolean isPalindrome(string pal) {
-		if(pal.equals(pal.reverse())){
+		
+	String reverse = "";
+        
+        for(int i = pal.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + pal.charAt(i);
+        }
+        
+		if(pal.equals(reverse)){
 			return true;	
 		}
 		else{
