bfb9ecfb19030c16d1b439363310f64e4f404e50
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-07-01 15:29:48 -0400

    Update SeleniumTest.java

--- src/test/java/pal/SeleniumTest.java
+++ src/test/java/pal/SeleniumTest.java
@@ -15,7 +15,7 @@ public class SeleniumTest {
 
     @Test()
     public void browserInitTest() {
-            System.setProperty("webdriver.chrome.driver","C:\DevOps\applications\chromedriver_win32\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver","C:/DevOps/applications/chromedriver_win32/chromedriver.exe");
             driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
