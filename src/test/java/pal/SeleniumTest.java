b49399033cc94f40423830253d5999b529a25fc1
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-07-02 08:31:06 -0400

    Update SeleniumTest.java

--- src/test/java/pal/SeleniumTest.java
+++ src/test/java/pal/SeleniumTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
