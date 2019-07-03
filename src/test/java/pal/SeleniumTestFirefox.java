5f22875de7f39698171ba2f3afe928bb0d5a8405
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-07-03 08:27:29 -0400

    Update SeleniumTestFirefox.java

--- src/test/java/pal/SeleniumTestFirefox.java
+++ src/test/java/pal/SeleniumTestFirefox.java
@@ -69,6 +69,28 @@ public class SeleniumTestFirefox {
         driver.close();
         
     }
+            @Test()
+    public void TestSpoonerismLinkExtended() {
+            this.setup();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        driver.get("http://localhost:8083/DevOpsDemo/");
+
+        WebElement link = driver.findElement(By.linkText("convert to spoonerism?"));
+        link.click();
+        WebElement word1 = driver.findElement(By.name("word1"));
+        WebElement word2 = driver.findElement(By.name("word2"));
+        word1.sendKeys("burning");
+        word2.sendKeys("rubber");
+        WebElement submit = driver.findElement(By.name("submit"));
+        submit.click();
+        String bodyText = driver.findElement(By.tagName("body")).getText();                                       
+        assertTrue(bodyText ,bodyText.contains("the spoonerism is: rurning bubber"));
+        driver.close();
+        
+    }
+}
         @AfterClass 
     public static void exit() {
           driver.quit();
