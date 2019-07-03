20f3d002a1c6a895bf08e4388c1ca19d082756c8
DG069474 <51761647+DG069474@users.noreply.github.com>
2019-07-03 14:12:43 -0400

    Update SeleniumTestIE.java

--- src/test/java/pal/SeleniumTestIE.java
+++ src/test/java/pal/SeleniumTestIE.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import java.util.concurrent.TimeUnit;
 
-public class SeleniumTestFirefox {
+public class SeleniumTestIE {
 
     private static WebDriver driver;
     
